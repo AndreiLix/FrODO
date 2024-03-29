e3c584aec475f34bb71b2390f1744f7ef99c1dfd
AndreiLix <andrei.lixandru.ilc@gmail.com>
2024-03-29 17:31:00 +0100

    finalized v0 utils_ANNs

--- FOLDER_code/comdo/utils_ANNs.py
+++ FOLDER_code/comdo/utils_ANNs.py
@@ -1,51 +1,18 @@
-
-import numpy as np
+import jax.numpy as jnp
 from matplotlib import pyplot as plt
 import copy
 
 
 
-def get_gradientVector_Autograd(f, x): 
-
-    """
-    Parameters
-    -----------
-    f: function
-        An arbitrary function.
-    x: array-like
-        An array with the (multi-dimensional) point at which the subgradient vector of f is desired.
-  
-        
-    Returns
-    ------- 
-    numpy array: shape (len(x), 1)
-        Gradient vector of f at point x.
-            Contains the partial derivatives of f with respect to x_1, ..., x_n.    
-
-    """
-    
-    import jax
-    import jax.numpy as jnp
-
-    Df = np.zeros_like(x, dtype= np.float64)
-
-    x1, x2 = x[0, 0], x[1, 0]
-
-    f_partial_x1 = jax.grad(f, argnums= 0)
-    f_partial_x2 = jax.grad(f, argnums= 1)
-
-    Df[0, 0] = f_partial_x1(x1, x2)
-    Df[1, 0] = f_partial_x2(x1, x2)
-       
-    return Df
 
 def fractional_v2(x, len_memory, _lambda= 0.15):
+
     """
     _lambda: float in (0, 1)
         The fractional order to use. Must be in (0,1).
 
     This version of fractional weighting is the discreized version of the Riemann-Liouville version.
-      Also found in "Discrete Fractional order PID controller". 
+        Also found in "Discrete Fractional order PID controller". 
     """
 
     from scipy.special import gamma
@@ -57,41 +24,55 @@ def fractional_v2(x, len_memory, _lambda= 0.15):
 
 class DOptimizer():
 
-    def __init__(self, models, fs_private, memory_len, beta_c, beta_g, beta_gm):
+    def __init__(self, models, fs_private, len_memory= 100, _lambda= 0.15, beta_c = 0.2, beta_cm= 0.04, beta_g= 0.6, beta_gm= 0.36):
         
+        """
+        self.idx_layersWithWeights
+            The 
+
+        """
+
         self.models = models
         self.fs_private = fs_private
-        self.len_memory = len(models)
-        self.memory_len = memory_len
+        self.n_agents = len(models)
+        self.len_memory = len_memory
+        self._lambda = _lambda
         self.beta_c = beta_c
         self.beta_g = beta_g
         self.beta_gm = beta_gm
+        self.idx_layersWithWeights = []
+        
+        self.shape_gradient_memory = [self.n_agents, self.len_memory]
+        self.shape_z_g = [self.n_agents]
+
+        for i in range(len(models[0].layers)):
+            if hasattr(models[0].layers[i], "weight"):
+                self.idx_layersWithWeights.append(i)
+                for dim in jnp.shape(models[0].layers[i]):
+                    self.shape_gradient_memory.append(dim)
+                    self.z_g.append(dim)
 
 
-    def step_withMemory(x, consensus_memory, gradient_memory, fs_private, scaled_memory= False, memory_profile= "exponential", b= 3, _lambda= 0.15, len_memory= 10, beta_c = 0.2, beta_cm= 0.04, beta_g= 0.6, beta_gm= 0.36):
+
+    def step_withMemory(self, models, grads_list):
+        
         """
 
         Changes the states (x), consensus_memory and gradient_memory of our agents based on a step in the optimization process.
 
         Parameters
         ----------
-        x: np array, shape (n_agents, n_params, 1).
+        x: jnp array, shape (n_agents, n_params, 1).
             Agent states.
 
-        consensus_memory: np array, shape (n_agents, n_params, len_memory)
-            Memory of consensus error for each parameter of length len_memory.
-            Memory encoded such that:
-                consensus_memory[agent_i][param_i][0] = consensus error len_memory iterations ago.
-                consensus_memory[agent_i][param_i][-1] = consensus error in the previous iteration.
-
-        gradient_memory: np array, shape (n_agents, n_params, len_gradientMemory)
-            Memory of past private subgradient values for each parameter of length len_gradientMemory.
+        gradient_memory: jnp array, shape (n_agents, self.len_memory, n_layers, <layer shape>)
+            Memory of past private subgradient values for each parameter of length self.len_memory.
                 Memory encoded such that:
-                gradient_memory[agent_i][param_i][0] = gradient len_memory iterations ago.
-                gradient_memory[agent_i][param_i][-1] = gradient in the previous iteration.
+                gradient_memory[agent_i][0] = gradients self.len_memory iterations ago.
+                gradient_memory[agent_i][-1] = gradients in the previous iteration.
 
-        b, _lambda: 
-            Scalar to parametrize the exponential and fractional memory profiles
+        _lambda: 
+            Scalar to parametrize the fractional memory profile.
                 
             
         scaled_memory: bool
@@ -106,44 +87,38 @@ class DOptimizer():
         """
 
 
-        n_agents = len(fs_private)
+        n_agents = len(self.fs_private)
         n_params = len(x[0])
 
         #---------------------------- first stage --------------------------------------
 
         # computing the terms used for the updates in this iteration
 
-        consesus_term = np.zeros([n_agents, n_params,1])
-        gradient_term = np.zeros([n_agents, n_params,1])
 
+        x = copy.deepcopy(models)
 
-        for agent_i in range(n_agents):
-            for param_i in range(n_params):
-                consesus_term[agent_i][param_i][0] = beta_c * sum( [ (x[agent_j][param_i][0] - x[agent_i][param_i][0]) for agent_j in range(n_agents) if agent_j!= agent_i ] )
+        consesus_term = jnp.zeros(self.shape_z_g)
+        gradient_term = jnp.zeros(self.shape_z_g)
+
         
         for agent_i in range(n_agents):
-            gradient_term[agent_i] = get_gradientVector_Autograd(fs_private[agent_i], x[agent_i])
+            for layer_i in self.idx_layersWithWeights:
+
+               gradient_term[agent_i][layer_i] = grads_list.layers[layer_i].weight
 
 
-
-        if memory_profile == "fractional_v2":
-            memory_weights = np.array([fractional_v2(x, len_memory, _lambda) for x in range(len_memory)])
-            # scaling values between 0 and 1
-            memory_weights = memory_weights / max(memory_weights)
+        memory_weights = jnp.array([fractional_v2(x, self.len_memory, self._lambda) for x in range(self.len_memory)])
+        # scaling values between 0 and 1
+        memory_weights = memory_weights / max(memory_weights)
 
 
-
-        z_g = np.zeros([n_agents, n_params, 1])
-
+        z_g = jnp.zeros([n_agents, n_params, 1])
 
         for agent_i in range(n_agents):
-            for param_i in range(n_params):           
-                
-                if scaled_memory:
-                    z_g[agent_i][param_i][0] = (1/len_memory) * sum([ memory_weights[memory_i] * gradient_memory[agent_i][param_i][memory_i] for memory_i in range(len_memory)])
 
-                if scaled_memory == False:
-                    z_g[agent_i][param_i][0] = sum([ memory_weights[memory_i] * gradient_memory[agent_i][param_i][memory_i] for memory_i in range(len_memory)])
+            aux_tensor = jnp.array([ memory_weights[memory_i] * self.gradient_memory[agent_i][memory_i] for memory_i in range(self.len_memory)])
+            
+            z_g[agent_i] = jnp.sum(aux_tensor, axis= 1)  # summing over the memory axis
 
 
 
@@ -159,16 +134,17 @@ class DOptimizer():
         # NOTE: all terms in update must be shape (n_agents, n_params, 1)
 
         for agent_i in range(n_agents):
-            for param_i in range(n_params):
-                x[agent_i][param_i][0] =  x[agent_i][param_i][0] \
-                                            + beta_c * consesus_term[agent_i][param_i][0] \
-                                            - beta_g * gradient_term[agent_i][param_i][0] \
-                                            - beta_gm * z_g[agent_i][param_i][0]
+            for layer_i in self.idx_layersWithWeights:
+
+                x[agent_i][layer_i] =  x[agent_i][layer_i] \
+                                            + self.beta_c * consesus_term[agent_i][layer_i] \
+                                            - self.beta_g * gradient_term[agent_i][layer_i] \
+                                            - self.beta_gm * z_g[agent_i][layer_i]
                 
                 # updating consensus mamory and gradient based on this iteration's sonsensus term and gradient descent term
 
-                gradient_memory[agent_i][param_i][:-1] = gradient_memory[agent_i][param_i][1:]
-                gradient_memory[agent_i][param_i][-1] = gradient_term[agent_i][param_i][0]
+                self.gradient_memory[agent_i][layer_i][:-1] = self.gradient_memory[agent_i][layer_i][1:]
+                self.gradient_memory[agent_i][layer_i][-1] = gradient_term[agent_i][layer_i]
 
 
-        return x, consensus_memory, gradient_memory
+        return x
